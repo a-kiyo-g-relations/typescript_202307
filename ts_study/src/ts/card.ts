457a7c7f80486c9ed4fab20c3966266e96237108
R-MORIYA\r-mor <r-moriya@g-relations.jp>
2023-07-11 19:48:31 +0900

    #76 コメント修正対応：card.ts改変

--- ts_study/src/ts/card.ts
+++ ts_study/src/ts/card.ts
@@ -1,14 +1,33 @@
-// 4種のマークを列挙型にする
-export enum SymbolMark {
-  SPADE,
-  HEART,
-  CLOVER,
-  DIAMOND,
+export namespace Card {
+  /**
+   * カードの数字の最小値を定義
+   */
+  export const MIN_NUMBER: number = 1;
+  /**
+   * カードの数字の最大値を定義
+   */
+  export const MAX_NUMBER: number = 13;
+  /**
+   * マークの型を定義
+   */
+  export type SymbolMark = string;
+  /**
+   * マークの配列を定義
+   */
+  export const SYMBOL_MARKS: SymbolMark[] = [
+    "SPADE",
+    "HEART",
+    "CLOVER",
+    "DIAMOND",
+  ];
+
+  /**
+   * カード1枚分のデータの型定義
+   */
+  export type elements = {
+    // カードの数字
+    readonly num: number;
+    // マーク
+    readonly symbol: SymbolMark;
+  };
 }
-// カード1枚の型を作成
-export type CardElement = {
-  // カードの数字
-  readonly num: number;
-  // マーク
-  readonly symbol: SymbolMark;
-};
--- ts_study/src/ts/deck.ts
+++ ts_study/src/ts/deck.ts
@@ -1,54 +1,50 @@
 // Cardクラスのインポート
-import { CardElement, SymbolMark } from "./card";
+import { Card } from "./card";
 
 export class Deck {
-  /**カード1枚のデータ
-   * @type { CardElement }
+  /**
+   * デッキ1組の変数を定義
    */
-  private card: CardElement;
-  /**デッキ1組
-   * @type { CardElement[] }
-   */
-  private cardMembers: CardElement[];
+  private cardMembers: Card.elements[];
 
   constructor() {
-    this.makeDeck();
+    this.cardMembers = this.makeDeck();
   }
 
-  /**デッキを作るメソッド
-   * @type { void }
+  /**
+   * デッキを作るメソッド
+   * @return 作成したデッキのデータ
    */
   makeDeck() {
-    // マーク4種類分のループ(i)、1~13の数字のループ(j)
-    for (let i = 0; i < 4; i++) {
-      for (let j: number = 1; j <= 13; j++) {
-        // 各マークに1~13の数字を割り当てる
-        switch (i) {
-          case 0:
-            this.card = { num: j, symbol: SymbolMark.SPADE };
-            break;
-          case 1:
-            this.card = { num: j, symbol: SymbolMark.HEART };
-            break;
-          case 2:
-            this.card = { num: j, symbol: SymbolMark.CLOVER };
-            break;
-          case 3:
-            this.card = { num: j, symbol: SymbolMark.DIAMOND };
-            break;
-          default:
-            break;
-        }
-        // デッキを定義する配列にプッシュする
-        this.cardMembers.push(this.card);
+    // マークの配列を取得
+    const symbolArray: Card.SymbolMark[] = Card.SYMBOL_MARKS;
+    // カードの配列を定義
+    const cardArray: Card.elements[] = [];
+    // マークそれぞれに対し1~13の数字のカードを作成し、配列に入れる
+    symbolArray.forEach((symbol) => {
+      for (let i = Card.MIN_NUMBER; i <= Card.MAX_NUMBER; i++) {
+        const card = { num: i, symbol: symbol };
+        cardArray.push(card);
       }
-    }
+    });
+    const shffledDeck = this.shuffleDeck(cardArray);
+    return shffledDeck;
   }
 
-  /**デッキ1組のデータを返すメソッド
-   * @returns { CardElement[] }
+  /**
+   * デッキ1組のデータを返すメソッド
+   * @returns デッキ1組のデータを返す
    */
-  getCardMembers() {
+  getCardMembers(): Card.elements[] {
     return this.cardMembers;
+  }
+
+  /**
+   * デッキをシャッフルするメソッド
+   * @return シャッフルしたカードの配列（デッキ）
+   */
+  shuffleDeck(cardArray: Card.elements[]): Card.elements[] {
+    cardArray.sort((a, b) => 0.5 - Math.random());
+    return cardArray;
   }
 }
