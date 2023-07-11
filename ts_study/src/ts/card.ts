bee9203d193873f91d6f8a2fced5504830311b65
R-MORIYA\r-mor <r-moriya@g-relations.jp>
2023-07-11 14:15:14 +0900

    #75 コメント追加、カードクラス削除

--- ts_study/src/ts/card.ts
+++ ts_study/src/ts/card.ts
@@ -7,14 +7,8 @@ export enum SymbolMark {
 }
 // カード1枚の型を作成
 export type CardElement = {
+  // カードの数字
   num: number;
+  // マーク
   symbol: SymbolMark;
 };
-// カード1枚分のデータを定義するクラス
-export class Card {
-  private card: CardElement;
-
-  constructor(num: number, symbol: SymbolMark) {
-    this.card = { num, symbol };
-  }
-}
